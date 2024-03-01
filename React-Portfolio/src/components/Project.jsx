2d188504510ad1ae61fecce4bc77c64e07e0fac3
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-01 19:59:27 +0000

    added function to project jsx

---
+++ React-Portfolio/src/components/Project.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Project = ({ title, deployedLink, githubLink, image }) => {
+  return (
+    <div className="project">
+      <h2>{title}</h2>
+      <img src={image} alt={title} />
+      <p>Link: <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed Version</a></p>
+      <p>GitHub: <a href={githubLink} target="_blank" rel="noopener noreferrer">GitHub Repository</a></p>
+    </div>
+  );
+};
+
+export default Project;
