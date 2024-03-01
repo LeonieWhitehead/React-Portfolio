51f64de82467d5d60e537bf6ad24948a20463395
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-01 20:23:04 +0000

    added main file with imports and render

--- React-Portfolio/src/components/Project.jsx
+++ React-Portfolio/src/components/Project.jsx
@@ -5,7 +5,7 @@ const Project = ({ title, deployedLink, githubLink, image }) => {
   return (
     <div className="project">
       <h2>{title}</h2>
-      <img src={image} alt={title} />
+      <img src={image} alt=""/>
       <p>Link: <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed Version</a></p>
       <p>GitHub: <a href={githubLink} target="_blank" rel="noopener noreferrer">GitHub Repository</a></p>
     </div>
