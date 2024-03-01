a4c80cad873b125c6be0c12d47619dce81690c6c
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-01 19:24:40 +0000

    added import for router and paths for routes

--- React-Portfolio/src/components/app.jsx
+++ React-Portfolio/src/components/app.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import ProjectGallery from './ProjectGallery';
 import Contact from './Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/projects" component={ProjectGallery} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
