460f1f5cf288a196652f685f548e6e73df7fe2d7
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-01 19:42:59 +0000

    added basic function for home page

---
+++ React-Portfolio/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Leonie Whitehead</h1>
+      {/* headshot */}
+      {/* brand statement */}
+    </div>
+);
+};
+
+export default Home;
