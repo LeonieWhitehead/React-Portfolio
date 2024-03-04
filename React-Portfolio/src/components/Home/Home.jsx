e5b51974fe84b09d7e8a3608ead7600f1740be63
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-04 19:35:48 +0000

    added headshot and styling to homepage

--- React-Portfolio/src/components/Home/Home.jsx
+++ React-Portfolio/src/components/Home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Home.css';
 import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
+import Vector1 from '../assets/Vector1.png';
+import Portrait from '../assets/Portrait.png';
+import Vector2 from '../assets/Vector2.png'
 const Home = () => {
 return(
   <div className='homepage'>
@@ -11,7 +14,7 @@ return(
         <span>A Front End Web Developer who strives to build immersive and beautiful web applications through carefully crafted code and user-centric design.</span>
       </div>
 
-      <button className='button h-button'>Hire me</button>
+      <button className='button h-button'>Portfolio</button>
 
       <div className='h-social-icons'>
       <a href="https://github.com/LeonieWhitehead" target="_blank" rel="noopener noreferrer" aria-label="GitHub Profile"><IoLogoGithub className='h-icon'/></a>
@@ -19,9 +22,10 @@ return(
       <a href="https://www.linkedin.com/in/leonie-whitehead/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile"><IoLogoLinkedin className='h-icon'/></a>
       </div>
     </div>
-    
+
     <div className='h-right'>
-    i am right side
+    <img src={Vector2} alt="" />
+    <img src={Portrait} alt="" />
     </div>
   </div>
 )
