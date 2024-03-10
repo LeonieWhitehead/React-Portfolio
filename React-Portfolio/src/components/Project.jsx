339abba1d9136734e072f028572fc39db8b3427a
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-10 16:24:08 +0000

    updated code on project jsx so that each slide can be clicked and then you can access the deployed link and github repo

--- React-Portfolio/src/components/Project.jsx
+++ React-Portfolio/src/components/Project.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import projectsData from '../projects.json';
-import { Carousel } from 'react-bootstrap';
+import projectsData from '../projects.json'; // Import project data
+import DelishDiscoveriesImage from './assets/DelishDiscoveries.png';
+import ConsoleFinancesImage from './assets/ConsoleFinances.png';
+import WorkDaySchedulerImage from './assets/WorkDayScheduler.png';
+import JavaScriptCodeQuizImage from './assets/JavaScriptCodeQuiz.png';
+import PasswordGeneratorImage from './assets/PasswordGenerator.png';
+import BootstrapPortfolioImage from './assets/BootstrapPortfolio.png';
+
+// Object mapping project titles to image imports
+const imageImports = {
+  'Delish Discoveries': DelishDiscoveriesImage,
+  'Console Finances': ConsoleFinancesImage,
+  'Work Day Scheduler': WorkDaySchedulerImage,
+  'JavaScript Code Quiz': JavaScriptCodeQuizImage,
+  'Password Generator': PasswordGeneratorImage,
+  'Bootstrap Portfolio': BootstrapPortfolioImage,
+};
 
 const Project = () => {
   const { projectId } = useParams();
   const project = projectsData.find((p) => p.id === parseInt(projectId, 10));
 
   if (!project) {
-    return <div>Project not found</div>; // Handle case where project is not found
+    return <div>Project not found</div>;
   }
 
-  const { title, deployedLink, githubLink, image, description } = project;
-  
+  const { title, deployedLink, githubLink, description } = project;
+
+  // Dynamically retrieve image import based on project title
+  const image = imageImports[title];
+
   return (
     <div className="project-details">
       <h2>{title}</h2>
-      <Carousel>
-        {images.map((image, index) => (
-          <Carousel.Item key={index}>
-            <img
-              className="d-block w-100"
-              src={image.src}
-              alt={image.alt}
-            />
-            <Carousel.Caption>
-              <h3>{image.caption}</h3>
-              <p>{image.description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      <img src={image} alt={title} />
       <p>{description}</p>
       <p>
         Link: <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed Version</a>
