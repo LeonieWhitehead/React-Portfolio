b7064670a911fddebd89182a821f2b903d5ff14a
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-03-10 16:59:33 +0000

    updated the styling of the contact form

--- React-Portfolio/src/components/Contact/Contact.jsx
+++ React-Portfolio/src/components/Contact/Contact.jsx
@@ -2,20 +2,12 @@ import { useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import ContactForm from './ContactForm';
 
-const Contact = () => {
-  const navigate = useNavigate();
-
-  const handleContactClick = () => {
-    navigate('/contact-form');
-  };
-
   const Contact = () => {
     return (
       <div className="contact">
-        <h2>Contact</h2>
         <ContactForm />
       </div>
     );
   };
-}
+
   export default Contact;
